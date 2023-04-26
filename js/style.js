7eaaa4efe624193fa0d289ef3bcf553215c36ec1
ShivamTaneja <shiivamtaneja@gmail.com>
2023-04-26 14:19:26 +0530

    add start and reset options

--- js/style.js
+++ js/style.js
@@ -1,10 +1,24 @@
 var canvas = document.querySelector("canvas");
-
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-
-
 var c = canvas.getContext("2d");
+
+let counter = 0;
+
+c.font = "50px Arial";
+c.fillStyle = "white";
+c.textAlign = "center";
+c.fillText("Press Any Key to Start", canvas.width / 2, canvas.height / 2);
+
+var keypress = document.addEventListener("keydown", handdleStart)
+
+function handdleStart() {
+    counter++;
+    if (counter <= 1) {
+        animate()
+        generateRandomCircles()
+    }
+}
 
 function Circle(x, y, dx, dy, r, colorIndex) {
     this.x = x;
@@ -14,15 +28,11 @@ function Circle(x, y, dx, dy, r, colorIndex) {
     this.r = r;
     this.colorIndex = colorIndex;
     this.update = function () {
-
         c.beginPath();
         c.arc(this.x, this.y, this.r, 0, 360); //(x,y,r,stAngle,endAngle)
         c.fillStyle = color[this.colorIndex];
         c.fill();
         c.closePath();
-
-
-
         if (this.x > window.innerWidth || this.x < 0) {
             this.x = Math.random() * window.innerWidth;
 
@@ -33,53 +43,42 @@ function Circle(x, y, dx, dy, r, colorIndex) {
             this.colorIndex = parseInt(Math.random() * color.length);
             this.r = (Math.random() * 30) + 10;
         }
-
         if (this.y > window.innerHeight || this.y < 0) {
             this.y = Math.random() * window.innerHeight;
-
             this.dx = (Math.random() - 0.5) * 5;
             this.dy = (Math.random() - 0.5) * 5;
-
             (Math.random() > 0.5) ? this.x = 0 : this.x = window.innerWidth;
             this.colorIndex = parseInt(Math.random() * color.length);
             this.r = (Math.random() * 30) + 10;
         }
-
         this.y += this.dy;
         this.x += this.dx;
-
     }
-
 }
 var color = ["#ffff01", "#ffc801", "#ff9000", "#ff6400", "#fe0000", "#c8007d", "#c8007d", "#6400a0", "#0011a9", "#005eb5", "#01a0c8", "#0eaf01", "#8bc800"];
-
 var circleArray = [];
+function generateRandomCircles() {
+    for (var i = 0; i < 10; i++) {
+        var x = window.innerWidth;
+        var y = window.innerHeight;
+        var dx = (Math.random() - 0.5) * 5;
+        var dy = (Math.random() - 0.5) * 5;
+        var r = (Math.random() * 30) + 10;
 
-for (var i = 0; i < 100; i++) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
-    var dx = (Math.random() - 0.5) * 5;
-    var dy = (Math.random() - 0.5) * 5;
-    var r = (Math.random() * 30) + 10;
+        var colorIndex = parseInt(Math.random() * color.length);
 
-    var colorIndex = parseInt(Math.random() * color.length);
-
-    circleArray.push(new Circle(x, y, dx, dy, r, colorIndex));
+        circleArray.push(new Circle(x, y, dx, dy, r, colorIndex));
+    }
 }
-
-
-//-------PLAYER------------
 
 var mouse = {
     x: canvas.width / 2,
     y: canvas.height / 2
 }
-
 window.addEventListener("mousemove", (e) => {
     mouse.x = e.pageX;
     mouse.y = e.pageY;
 })
-
 var player = {
     radius: 20,
     increment: 0,
@@ -87,12 +86,9 @@ var player = {
 };
 
 function animate() {
-    requestAnimationFrame(animate);
-
     c.beginPath();
     c.clearRect(0, 0, canvas.width, canvas.height);
     c.closePath();
-
     for (var i = 0; i < circleArray.length; i++) {
         circleArray[i].update();
         var dist = Math.sqrt(Math.pow(circleArray[i].x - mouse.x, 2) + Math.pow(circleArray[i].y - mouse.y, 2));
@@ -115,7 +111,6 @@ function animate() {
         }
     }
 
-
     c.beginPath();
     c.arc(mouse.x, mouse.y, player.radius, 0, 360);
     c.fillStyle = "white";
@@ -128,11 +123,23 @@ function animate() {
     }
 
     if (player.finish) {
-        alert("gameOver");
-        return;
+        cancelAnimationFrame(animate);
+        reset()
+        return
     }
 
-
+    requestAnimationFrame(animate);
 
 }
-animate();
+
+function reset() {
+    c.font = "50px Arial";
+    c.fillStyle = "white";
+    c.textAlign = "center";
+    c.fillText("Game Over", canvas.width / 2, (canvas.height / 2) - 100);
+    c.fillText("Press Any Key to Reset", canvas.width / 2, canvas.height / 2);
+    document.addEventListener("keydown", () => {
+        document.location.reload(true)
+
+    })
+}
